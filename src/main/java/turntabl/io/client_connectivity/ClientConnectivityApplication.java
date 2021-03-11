f347d2b28334da6a42cd62599a751525b6e88852
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-03-11 19:03:19 +0000

    setup gradle project

---
+++ src/main/java/turntabl/io/client_connectivity/ClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package turntabl.io.client_connectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/turntabl/io/client_connectivity/ClientConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.client_connectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
